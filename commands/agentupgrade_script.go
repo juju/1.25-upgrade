e3496632099ef1928dee6e9b44d465f345a7d13a
Christian Muirhead <christian.muirhead@canonical.com>
2017-10-04 16:04:53 +1300

    Save and rollback for init files for container agents
    Since we're renaming all of the container machine agents from lxc -> lxd
    we need to keep the files around for rollback.

--- commands/agentupgrade_script.go
+++ commands/agentupgrade_script.go
@@ -17,6 +17,7 @@ import json
 import os
 from os import path
 import shutil
+import subprocess
 import sys
 import tarfile
 import yaml
@@ -32,8 +33,13 @@ API_ADDRESSES = """{{range .ControllerInfo.Addrs}}{{.}}
 
 BASE_DIR = '/var/lib/juju'
 ROLLBACK_DIR = path.join(BASE_DIR, '1.25-upgrade-rollback')
+ROLLBACK_INIT_DIR = path.join(ROLLBACK_DIR, 'init')
 TOOLS_DIR = path.join(BASE_DIR, 'tools')
 AGENTS_DIR = path.join(BASE_DIR, 'agents')
+INIT_DIR = path.join(BASE_DIR, 'init')
+
+UPSTART_DIR = '/etc/init'
+SYSTEMD_DIR = '/etc/systemd/system'
 
 UPGRADE_DIR, SCRIPT = path.split(__file__)
 
@@ -109,7 +115,8 @@ def convert_lxd_agent(agent):
     return convert_container_agent(agent, 'lxd', 'lxc')
 
 def save_rollback_info():
-    os.mkdir(ROLLBACK_DIR)
+    os.makedirs(ROLLBACK_INIT_DIR)
+    series = get_series()
     for agent in all_agents():
         tools_link = path.join(TOOLS_DIR, agent)
         target = os.readlink(tools_link)
@@ -118,6 +125,22 @@ def save_rollback_info():
         agent_conf = path.join(AGENTS_DIR, agent, 'agent.conf')
         backup_path = path.join(ROLLBACK_DIR, agent + '_agent.conf')
         shutil.copy(agent_conf, backup_path)
+
+        if series == 'trusty':
+            agent_init = path.join(UPSTART_DIR, upstart_conf(agent))
+            shutil.copy(agent_init, ROLLBACK_INIT_DIR)
+        else:
+            # Grab the service symlink...
+            service = systemd_conf(agent)
+            init_link = path.join(SYSTEMD_DIR, service)
+            target = os.readlink(init_link)
+            os.symlink(target, path.join(ROLLBACK_INIT_DIR, service))
+
+            # ...And the init subdir.
+            dirname = 'jujud-' + agent
+            agent_init_dir = path.join(INIT_DIR, dirname)
+            saved_dir = path.join(ROLLBACK_INIT_DIR, dirname)
+            shutil.copytree(agent_init_dir, saved_dir)
 
 def find_new_tools():
     files = [name for name in os.listdir(UPGRADE_DIR) if path.join(UPGRADE_DIR, name).endswith('.tgz')]
@@ -151,9 +174,7 @@ def install_tools():
 def make_links(in_dir, names, target):
     for name in names:
         link_path = path.join(in_dir, name)
-        if path.exists(link_path):
-            os.unlink(link_path)
-        os.symlink(target, link_path)
+        force_symlink(target, link_path)
 
 def update_configs():
     for agent in all_agents():
@@ -209,14 +230,69 @@ def update_unit_config(agent, data):
 
     return data
 
+def get_series():
+    return subprocess.check_output(['/usr/bin/lsb_release', '-cs']).decode().strip()
+
 def main():
     assert not path.exists(ROLLBACK_DIR), 'saved rollback information found - aborting'
     save_rollback_info()
     install_tools()
     update_configs()
 
+def safe_unlink(location):
+    if path.exists(location):
+        os.unlink(location)
+
+def force_symlink(target, dest):
+    safe_unlink(dest)
+    os.symlink(target, dest)
+
+def upstart_conf(agent):
+    return 'jujud-{}.conf'.format(agent)
+
+def systemd_conf(agent):
+    return 'jujud-{}.service'.format(agent)
+
+def rollback_upstart(lxd_agent, lxc_agent):
+    safe_unlink(path.join(UPSTART_DIR, upstart_conf(lxd_agent)))
+    saved_conf = path.join(ROLLBACK_INIT_DIR, upstart_conf(lxc_agent))
+    shutil.copy(saved_conf, UPSTART_DIR)
+
+def rollback_systemd(lxd_agent, lxc_agent):
+    # Get rid of any lxd version of the agent files under /var/lib/juju/init.
+    lxd_init_dir = path.join(INIT_DIR, 'jujud-' + lxd_agent)
+    if path.exists(lxd_init_dir):
+        shutil.rmtree(lxd_init_dir)
+
+    # Reinstate the lxc init files.
+    lxc_init_dir = path.join(INIT_DIR, 'jujud-' + lxc_agent)
+    if path.exists(lxc_init_dir):
+        shutil.rmtree(lxc_init_dir)
+    shutil.copytree(path.join(ROLLBACK_INIT_DIR, 'jujud-' + lxc_agent), lxc_init_dir)
+
+    # Get rid of any lxd symlink from /etc/systemd/system.
+    safe_unlink(path.join(SYSTEMD_DIR, systemd_conf(lxd_agent)))
+    # Reinstate the lxc link.
+    lxc_agent_conf = systemd_conf(lxc_agent)
+    force_symlink(path.join(lxc_init_dir, lxc_agent_conf), path.join(SYSTEMD_DIR, lxc_agent_conf))
+
+def rollback_init_files(series, lxd_agent, lxc_agent):
+    if series == 'trusty':
+        rollback_upstart(lxd_agent, lxc_agent)
+    else:
+        rollback_systemd(lxd_agent, lxc_agent)
+
+def reload_init(series):
+    if series == 'trusty':
+        command = ['/sbin/initctl', 'reload']
+    else:
+        command = ['/bin/systemctl', 'daemon-reload']
+    subprocess.check_call(command)
+
 def rollback():
     assert path.exists(ROLLBACK_DIR), 'no rollback information found'
+    series = get_series()
+    need_init_reload = False
     for agent in all_agents():
         lxd, lxc_agent = convert_lxd_agent(agent)
         if lxd:
@@ -225,17 +301,16 @@ def rollback():
             os.rename(
                 path.join(AGENTS_DIR, agent),
                 path.join(AGENTS_DIR, lxc_agent))
-            lxd_tools_link = path.join(TOOLS_DIR, agent)
-            if path.exists(lxd_tools_link):
-                os.unlink(lxd_tools_link)
+            safe_unlink(path.join(TOOLS_DIR, agent))
+
+            rollback_init_files(series, agent, lxc_agent)
+            need_init_reload = True
             agent = lxc_agent
 
         link_path = path.join(ROLLBACK_DIR, agent)
         target = os.readlink(link_path)
         dest = path.join(TOOLS_DIR, agent)
-        if path.exists(dest):
-            os.unlink(dest)
-        os.symlink(target, dest)
+        force_symlink(target, dest)
 
         agent_conf = path.join(AGENTS_DIR, agent, 'agent.conf')
         backup_path = path.join(ROLLBACK_DIR, agent + '_agent.conf')
@@ -245,6 +320,9 @@ def rollback():
     added_tools = path.join(TOOLS_DIR, tools_base)
     shutil.rmtree(added_tools)
     shutil.rmtree(ROLLBACK_DIR)
+
+    if need_init_reload:
+        reload_init(series)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "rollback":
