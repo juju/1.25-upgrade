e3652f4c3165336262be4f7da19543dfc01e314f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 14:28:19 +0000

    Adding tarfile member sanitization to extractall()

--- juju2/releasetests/winbuildtest.py
+++ juju2/releasetests/winbuildtest.py
@@ -92,7 +92,26 @@ def untar(tarball_path):
     error_message = None
     try:
         with tarfile.open(name=tarball_path, mode='r:gz') as tar:
-            tar.extractall(path=TMP_DIR)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=TMP_DIR)
     except tarfile.ReadError:
         error_message = "Not a tar.gz: %s" % tarball_path
         raise Exception(error_message)
